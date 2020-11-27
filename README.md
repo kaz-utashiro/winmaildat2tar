094eb84c0a53ca2922017e7e36ab51dc80818880
Kazumasa Utashiro <kaz@utashiro.com>
2020-11-27 22:43:28 +0900

    minil new App::winmaildat2tar

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::winmaildat2tar - It's new $module
+
+# SYNOPSIS
+
+    use App::winmaildat2tar;
+
+# DESCRIPTION
+
+App::winmaildat2tar is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/winmaildat2tar.pm
@@ -0,0 +1,39 @@
+package App::winmaildat2tar;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::winmaildat2tar - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::winmaildat2tar;
+
+=head1 DESCRIPTION
+
+App::winmaildat2tar is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
